9f0c1482689bc5dd4aacbdeba1ea6792cadfd15a
Shajith Chandran <aixdeveloper@gmail.com>
2018-12-27 15:08:10 +0530

    Initial implementation to use the pytorch onnx model and run it under onnx runtime.

---
+++ onnxruntime/infer.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../')
+from common.CreditDataSet import CreditDataSet
+import onnxruntime as rt
+import numpy
+
+dataset = CreditDataSet("../data/train.csv")
+inputs = dataset.getrawinputs()
+expected_output = dataset.getrawoutputs()
+
+rtmodel = rt.InferenceSession("../models/cust_credit.onnx")
+
+#print(rtmodel.__dict__)
+
+input_name = rtmodel.get_inputs()[0].name
+label_name = rtmodel.get_outputs()[0].name
+
+predicted_output = rtmodel.run([label_name], {input_name: inputs})
+
+print (predicted_output)
+
+diff = abs(expected_output - predicted_output[0])
+
+correct = 0
+total = 0
+for i in diff[0]:
+    total += 1
+    if i <0.2:
+        correct += 1
+
+print("Correct: ", correct, " Total: " , total)
+
